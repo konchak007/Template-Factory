8db1803250abba3918520b12adeae0bf6a0f165b
Konchak007 <markonchak@gmail.com>
2019-06-21 21:28:12 +0300

    [create]sceleton

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='search-field'>
+        <input/>
+      </div>
+      <div className='organization-list' ></div>
+
+    </div>
+  );
+}
+
+export default App;
