1455e3f5df9b782f682cc60923f9d4d5b5ed7db5
Konchak007 <markonchak@gmail.com>
2019-06-23 19:25:00 +0300

    organization card

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Search} />
-        <Route path="/organisation" component={() => <h1>asdasd</h1>} />
-        <Route path="/profile" component={() => <h1>Mariyka</h1>} />
+        <Route exact path="/" component={Search} />
+        <Route exact path="/organisation/:id" component={() => <h1>asdasd</h1>} />
+        <Route exact path="/profile" component={() => <h1>Mariyka</h1>} />
+        <Route component={() => <h1>404</h1>} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/OrganisationsList/OrganizationCard/OrganizationCard.js
+++ src/components/OrganisationsList/OrganizationCard/OrganizationCard.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const OrganizationCard = ({ id, name, avatar }) => {
   return (
-    <div>
-      <img src={avatar} width="100px" height="100px" />
-      {name}
-    </div>
+    <Link to={`/organisation/${id}`}>
+      <div>
+        <img src={avatar} width="100px" height="100px" />
+        {name}
+      </div>
+    </Link>
   );
 };
 export default OrganizationCard;
