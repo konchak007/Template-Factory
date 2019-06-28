848e752db3090ee73527c05f305d0489be44d3ec
Konchak007 <markonchak@gmail.com>
2019-06-28 15:29:56 +0300

    [upd] router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, HashRouter as Router } from "react-router-dom";
 
 import { Search, Organization, User } from "./pages";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Switch>
         <Route exact path="/" component={Search} />
         <Route exact path="/organisation/:name" component={Organization} />
         <Route exact path="/profile/:name" component={User} />
         <Route component={() => <h1>404</h1>} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
