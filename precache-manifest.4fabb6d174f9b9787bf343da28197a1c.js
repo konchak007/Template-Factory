4a55444e3239f8cf3bbb2bf9931bf5689bcaee68
Konchak007 <markonchak@gmail.com>
2019-06-28 15:25:17 +0300

    Updates

---
+++ precache-manifest.4fabb6d174f9b9787bf343da28197a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71327824f8b95ef6027c9536a0bef433",
+    "url": "/Template-Factory/index.html"
+  },
+  {
+    "revision": "c565594ad3ff881f64e2",
+    "url": "/Template-Factory/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "509436ad07045464873b",
+    "url": "/Template-Factory/static/css/main.da2e76ac.chunk.css"
+  },
+  {
+    "revision": "c565594ad3ff881f64e2",
+    "url": "/Template-Factory/static/js/2.d0bab29f.chunk.js"
+  },
+  {
+    "revision": "509436ad07045464873b",
+    "url": "/Template-Factory/static/js/main.20b193a8.chunk.js"
+  },
+  {
+    "revision": "463e6a4e51917eac0fd2",
+    "url": "/Template-Factory/static/js/runtime~main.f623c267.js"
+  }
+]);
